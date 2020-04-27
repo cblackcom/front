9960dc92ced6fa5192ccc6af26442726f29b98b1
CBlack 黑科林 <cblack@cblack.com>
2020-04-27 16:08:49 -0700

    Move npmignore back to gitignore after install

--- install.js
+++ install.js
@@ -2,5 +2,10 @@
 
 var path = require('path')
 var ncp = require('ncp').ncp
+var fs = require('fs')
 
 ncp(path.resolve(__dirname, 'template'), '.')
+
+if(fs.existsSync('.npmignore') && !fs.existsSync('.gitignore')) {
+	fs.renameSync('.npmignore', '.gitignore')
+}
